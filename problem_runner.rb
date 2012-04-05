bbfe384da6cf633843da9e015cfbda34b4e7928b
Anthony Sciamanna <asciamanna@gmail.com>
2012-04-05 13:39:13 -0400

    updated problem_runner

--- problem_runner.rb
+++ problem_runner.rb
@@ -1,2 +1,15 @@
-require "./problem2"
-Problem2.new.run
+print "Which Problem do you want to run?:"
+problem_number = STDIN.gets.to_i 
+if problem_number == 0 
+	puts "Invalid input.  You must enter a number."
+	exit
+end
+
+begin
+	require "./problem#{problem_number}"
+rescue Exception=>e
+	puts "Problem #{problem_number} does not exist."
+	exit
+end
+
+Module.const_get("Problem#{problem_number}").new.run
